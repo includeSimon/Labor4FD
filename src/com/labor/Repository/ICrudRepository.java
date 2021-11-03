dab29363741e70d9d735a731d9964b49949518d4
Pirlog Simon <simon.pirlog@yahoo.com>
2021-11-03 18:30:45 +0200

    modified ICrudRepository to throw NullException

--- src/com/labor/Repository/ICrudRepository.java
+++ src/com/labor/Repository/ICrudRepository.java
@@ -1,5 +1,7 @@
 package com.labor.Repository;
 
+import com.labor.Exceptions.NullException;
+
 import java.util.UUID;
 
 public interface ICrudRepository<T> {
@@ -7,8 +9,9 @@ public interface ICrudRepository<T> {
      * finds the object with id 'id'
      * @param id id of searched object
      * @return object with id or null if it doesn't exist
+     * @throws NullException if input parameter id is NULL
      */
-    T findOne(UUID id);
+    T findOne(UUID id) throws NullException;
 
 
     /**
@@ -22,23 +25,26 @@ public interface ICrudRepository<T> {
      * saves(adds) an object to repository
      * @param obj obj different from null
      * @return null if object is saved otherwise obje if id exists
+     * @exception NullException if obj is NULL
      */
-    T save(T obj);
+    T save(T obj) throws NullException;
 
 
     /**
      * modifies an object
      * @param obj obj different from null
-     * @return null - null if object is updated otherwise obj if id doesn't existL
+     * @return null - null if object is updated otherwise obj if id doesn't exist
+     * @exception NullException if obj is NULL
      */
-    T update(T obj);
+    T update(T obj) throws NullException;
 
 
     /**
      * removes object with id 'id'
      * @param id id must be not null
      * @return obj or null obj id doesn't exist
+     * @exception NullException if id is NULL
      */
-    T delete(UUID id);
+    T delete(UUID id) throws NullException;
 
 }
