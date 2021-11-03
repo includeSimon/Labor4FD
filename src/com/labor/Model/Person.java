e05ef52ba4f9cc575cef9d0667f66866002f0fb5
Pirlog Simon <simon.pirlog@yahoo.com>
2021-11-03 17:35:38 +0200

    finished class Person

--- src/com/labor/Model/Person.java
+++ src/com/labor/Model/Person.java
@@ -1,6 +1,34 @@
 package com.labor.Model;
 
-public class Person {
-    public String firstName;
-    public String lastName;
-}
+//Person class should be abstract because it is inherited by class 'Student' and 'Teacher'
+public abstract class  Person {
+    private String firstName;
+    private String lastName;
+    public Person(String firstName,String lastName){
+        this.firstName=firstName;
+        this.lastName=lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
