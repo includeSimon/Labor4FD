55d8d387d3e7a2a0af703691eb2c41134c9adaeb
Pirlog Simon <simon.pirlog@yahoo.com>
2021-11-03 14:37:18 +0200

    Finished Course class

--- src/com/labor/Model/Course.java
+++ src/com/labor/Model/Course.java
@@ -12,6 +12,69 @@ public class Course {
     private int credits;
     private UUID id;       //unique identifier used for comparison between objects of the same class 'Course'
 
+    public Course(String name, Person teacher, int maxEnrolment, int credits) {
+        this.name = name;
+        this.teacher = teacher;
+        this.maxEnrollment = maxEnrolment;
+        this.studentsEnrolled= new ArrayList<>();
+        this.credits = credits;
+        this.id=UUID.randomUUID();     //initializing a random id for each object
+    }
 
+    public String getName() {return name; }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Person getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Person teacher) {
+        this.teacher = teacher;
+    }
+
+    public int getMaxEnrolment() {
+        return maxEnrollment;
+    }
+
+    public void setMaxEnrolment(int maxEnrolment) {
+        this.maxEnrollment = maxEnrolment;
+    }
+    //com.labor.Model.Student
+    public List<Student> getStudentsEnrolled() {
+        return studentsEnrolled;
+    }
+
+    public void setStudentsEnrolled(List<Student> studentsEnrolled) {
+        this.studentsEnrolled = studentsEnrolled;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Course name = '" + name +
+                "', teacher = " + teacher +
+                ", maxEnrollment = " + maxEnrollment +
+                ", credits = " + credits +
+                ", studentsEnrolled=" + studentsEnrolled +
+                ", id=" + id;
+    }
 
 }
